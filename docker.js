46d5276a4342c1861aa70314ffabff22499cc0be
Martin Toman <whomenope@outlook.com>
2019-07-21 14:20:16 +0200

    Add eslint rules + lint

--- docker.js
+++ docker.js
@@ -1,60 +1,58 @@
-/* eslint-disable */
-
 const request = require('request')
 const { Docker } = require('node-docker-api')
 
-const docker = new Docker({ socketPath: '/var/run/docker.sock' })
+const docker = new Docker({socketPath: '/var/run/docker.sock'})
 
 let container = null
 let logstream = null
 
 function create(image) {
-    return docker.container.create({
-      Image: image,
-      Env: [
-        "PROGRAM_ALIAS=mailgun_integration_test",
-        "PORT=3000",
-        "LOG_FILE=false",
-        "LOG_LEVEL=debug",
-        "MAILGUN_API_KEY=api_key",
-        "MAILGUN_DOMAIN=mail.multipl.io",
+  return docker.container.create({
+    Image: image,
+    Env: [
+      "PROGRAM_ALIAS=mailgun_integration_test",
+      "PORT=3000",
+      "LOG_FILE=false",
+      "LOG_LEVEL=debug",
+      "MAILGUN_API_KEY=api_key",
+      "MAILGUN_DOMAIN=mail.multipl.io",
 
-        "NODE_ENV=development",
-        "http_proxy=http://127.0.0.1:3002",
-        "no_proxy=localhost,127.0.0.1"
-      ],
-      HostConfig: {
-        PortBindings: {
-          "3000/tcp": [{
-            "HostPort": "3000",
-          }],
-          "3002/tcp": [{
-            "HostPort": "3002",
-          }],
-        },
-        NetworkMode: "host",
+      "NODE_ENV=development",
+      "http_proxy=http://127.0.0.1:3002",
+      "no_proxy=localhost,127.0.0.1"
+    ],
+    HostConfig: {
+      PortBindings: {
+        "3000/tcp": [{
+          "HostPort": "3000",
+        }],
+        "3002/tcp": [{
+          "HostPort": "3002",
+        }],
       },
+      NetworkMode: "host",
+    },
+  })
+    .then(c => {
+      container = c
+      console.log('Container created')
+      return c.start()
     })
-      .then(c => {
-        container = c
-        console.log('Container created')
-        return c.start()
-      })
-      .then((c) => c.logs({
-        follow: true,
-        stdout: true,
-        stderr: true,
-      }))
-      .then(stream => {
-        logstream = stream
-        stream.on('data', info => console.log(info.toString()))
-        stream.on('error', err => console.log(err.toString()))
-      })
-      .then(() => {
-        console.log('Container started')
-        return ready("/ready")
-      })
-      .catch(error => console.log(error))
+    .then((c) => c.logs({
+      follow: true,
+      stdout: true,
+      stderr: true,
+    }))
+    .then(stream => {
+      logstream = stream
+      stream.on('data', info => console.log(info.toString()))
+      stream.on('error', err => console.log(err.toString()))
+    })
+    .then(() => {
+      console.log('Container started')
+      return ready("/ready")
+    })
+    .catch(error => console.log(error))
 }
 
 function ready(path) {
@@ -69,7 +67,7 @@ function ready(path) {
 
       request
         .get("http://127.0.0.1:3000" + path)
-        .on('response', function(response) {
+        .on('response', function (response) {
           if (response.statusCode == 200) {
             resolve(response)
           }
@@ -77,7 +75,7 @@ function ready(path) {
             setTimeout(check, timeout)
           }
         })
-        .on('error', (error) => {
+        .on('error', () => {
           setTimeout(check, timeout)
         })
     }
@@ -106,4 +104,3 @@ module.exports = {
   ready,
   kill,
 }
-
